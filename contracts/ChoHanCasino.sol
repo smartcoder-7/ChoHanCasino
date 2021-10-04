16c98542bb9831af45415ff589c9d729fd98ab27
Dream High <smartcoder0415@gmail.com>
2021-10-04 07:55:40 -0400

    changed logic

--- contracts/ChoHanCasino.sol
+++ contracts/ChoHanCasino.sol
@@ -2,122 +2,128 @@
 pragma solidity ^0.8.0;
 
 contract ChoHanCasino {
-  address payable public owner;
-  uint256 public minimumBet;
-  uint256 public totalBet;
-  uint256 public numberOfPlayers;
-  uint256 public minNumberPlayer = 2;
-  address[] public players;
-  uint public numberWinner;
+    address payable public owner;
+    uint256 public minimumBet;
+    uint256 public totalBet;
+    uint256 public minNumberPlayer = 2;
+    uint256 public numberWinner = 2;
+    bool ended;
 
-  struct Player {
-    uint256 amountBet;
-    uint256 numberSelected;
-  }
+    struct Player {
+        uint256 id;
+        uint256 amountBet;
+        uint256 numberSelected;
+        address payable playerAddress;
+    }
 
-  struct Bet {
-    uint numberWinner;
-    uint totalBet;
-  }
+    struct Bet {
+        uint256 numberWinner;
+        uint256 totalBet;
+    }
 
-  mapping(address => Player) public playerInfo;
-  Bet[] public betList;
-  event Won(bool _status, address _address, uint _amount);
+    Player[] public players;
+    Bet[] public bets;
 
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
+    event Won(bool _status, address _address, uint256 _amount);
 
-  constructor(uint256 _mininumBet) payable {
-    owner = payable(msg.sender);
-    if (_mininumBet != 0) 
-      minimumBet = _mininumBet;
-  }
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-  fallback() external payable {}
-  receive() external payable {}
+    constructor(uint256 _mininumBet) {
+        owner = payable(msg.sender);
+        if (_mininumBet != 0) minimumBet = _mininumBet;
+    }
 
-  function kill() public {
-    if (msg.sender == owner)
-      selfdestruct(owner);
-  }
+    fallback() external payable {}
 
-  function withdraw() public onlyOwner returns(bool) {
-    owner.transfer(address(this).balance);
-    return true;
-  }
+    receive() external payable {}
 
-  function checkPlayerExists(address player) public view returns(bool) {
-    for (uint256 i = 0; i < players.length; i++) {
-      if (players[i] == player)
+    function kill() public {
+        if (msg.sender == owner) selfdestruct(owner);
+    }
+
+    function withdraw() public onlyOwner returns (bool) {
+        owner.transfer(address(this).balance);
         return true;
     }
-    return false;
-  }
 
-  function bet(uint256 numberSelected) public payable {
-    require(!checkPlayerExists(msg.sender));
-    require(numberSelected == 1 || numberSelected == 0);
-    require(msg.value >= minimumBet);
-
-    playerInfo[msg.sender].amountBet = msg.value;
-    playerInfo[msg.sender].numberSelected = numberSelected;
-    numberOfPlayers++;
-    players.push(msg.sender);
-    totalBet += msg.value;
-    if (numberOfPlayers >= minNumberPlayer)
-      generateNumberWinner();
-  }
-
-   function generateNumberWinner() public {
-    uint256 numberGenerated = block.number % 36 + 1;
-    numberWinner = numberGenerated;
-    distributePrizes(numberGenerated);
-  }
-
-  function distributePrizes(uint256 numberWin) public {
-    address[100] memory winners;
-    address[100] memory losers;
-    uint256 countWin = 0;
-    uint256 countLose = 0;
-
-    for (uint256 i = 0; i < players.length; i++) {
-      address playerAddress = players[i];
-      if (playerInfo[playerAddress].numberSelected == numberWin) {
-        winners[countWin] = playerAddress;
-        countWin++;
-      } else {
-        losers[countLose] = playerAddress;
-        countLose++;
-      }
-      delete playerInfo[playerAddress];
-    }
-
-    if (countWin !=0) {
-      uint256 winnerEtherAmount = totalBet/countWin;
-
-      for (uint256 j = 0; j < countWin; j++) {
-        if (winners[j] != address(0)) {
-          payable(winners[j]).transfer(winnerEtherAmount);
-          emit Won(true, winners[j], winnerEtherAmount);
+    function checkPlayerExists(address player) public view returns (bool) {
+        for (uint256 i = 0; i <= players.length; i++) {
+            if (players[i].playerAddress == player) return true;
         }
-      }
+        return false;
     }
 
-    for (uint256 l =0; l < losers.length; l++) {
-      if (losers[l] != address(0))
-        emit Won(false, losers[l], 0);
+    function bet(uint256 _numberSelected) public payable {
+        //selected number has to be 1 or 0
+        // 1 for even 0 for odd
+        require(!checkPlayerExists(msg.sender));
+        require(_numberSelected <= 1);
+        require(msg.value >= minimumBet);
+
+        players.push(
+            Player(
+                players.length,
+                msg.value,
+                _numberSelected,
+                payable(msg.sender)
+            )
+        );
+
+        totalBet += msg.value;
     }
-    resetData();
-  }
 
+    function betEnd() public onlyOwner {
+        generateNumberWinner();
+        distributePrizes(numberWinner);
+        resetData();
+    }
 
-  function resetData() public {
-    delete players;
-    betList.push(Bet(numberWinner, totalBet));
-    totalBet = 0;
-    numberOfPlayers = 0;
-  }
+    function generateNumberWinner() public {
+        uint256 numberGenerated = ((block.number % 36) + 1) % 2;
+        numberWinner = numberGenerated;
+    }
+
+    function distributePrizes(uint256 numberWin) public {
+        address[100] memory winners;
+        address[100] memory losers;
+        uint256 countWin = 0;
+        uint256 countLose = 0;
+
+        for (uint256 i = 0; i < players.length; i++) {
+            address playerAddress = players[i].playerAddress;
+            if (players[i].numberSelected == numberWin) {
+                winners[countWin] = playerAddress;
+                countWin++;
+            } else {
+                losers[countLose] = playerAddress;
+                countLose++;
+            }
+            delete players[i];
+        }
+
+        if (countWin != 0) {
+            uint256 winnerEtherAmount = totalBet / countWin;
+
+            for (uint256 j = 0; j < countWin; j++) {
+                if (winners[j] != address(0)) {
+                    payable(winners[j]).transfer(winnerEtherAmount);
+                    emit Won(true, winners[j], winnerEtherAmount);
+                }
+            }
+        }
+
+        for (uint256 l = 0; l < losers.length; l++) {
+            if (losers[l] != address(0)) emit Won(false, losers[l], 0);
+        }
+    }
+
+    function resetData() public {
+        delete players;
+        bets.push(Bet(numberWinner, totalBet));
+        totalBet = 0;
+        numberWinner = 2;
+    }
 }
-
